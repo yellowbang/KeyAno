6867b2405842d3bd3acdd66f08f9acfdea064acc
yellowbang <astrotabon@gmail.com>
2014-06-10 01:47:39 -0700

    fsdfsd

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var url = require("url");
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        // url.parse(request.url).pathname is the web address.
+        console.log("Request for " + pathname + " received.");
+        //    route(handle, pathname);
+        route(handle, pathname,response);
+
+    }
+    http.createServer(onRequest).listen(3003);
+    console.log("Server has started.");
+}
+
+exports.start = start;
