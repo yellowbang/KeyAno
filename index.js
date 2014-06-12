ac74df48c9b9b53fa9ad92e91ed7216655d01004
yellowbang <astrotabon@gmail.com>
2014-06-12 11:37:04 -0700

    yes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs")
-port = Number(process.env.PORT || 3100);
+port = Number(process.env.PORT || 3000);
 
 http.createServer(function(request, response) {
 
